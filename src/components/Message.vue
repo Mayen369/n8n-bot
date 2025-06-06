41689f70875770eb2250462ace56c4508b28b466
Mayen369 <mayencompany@gmail.com>
2025-06-05 20:00:56 -0600

    Create Message.vue

---
+++ src/components/Message.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-if="message.text">
+    <slot name="beforeMessage" />
+    <div class="chat-message">{{ message.text }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.chat-message {
+  background-color: #f1f1f1;
+  padding: 10px;
+  margin: 5px 0;
+  border-radius: 4px;
+  font-family: sans-serif;
+}
+</style>
+fix: corregido Message.vue que causaba error de compilación
